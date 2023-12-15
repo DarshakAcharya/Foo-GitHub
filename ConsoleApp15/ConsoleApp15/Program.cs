8173bfcb30570101363634a9aa374e60cda11873
DarshakAcharya <darshakacharya2625@gmail.com>
2023-12-15 15:37:04 +0530

    Added feature2 in newly added branch feature2

--- ConsoleApp15/ConsoleApp15/Program.cs
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Edited Program.cs");
+
+Console.WriteLine("Added feature2");
